f1a3ec34d2d89eadd9a3d77212e7d7b701f1c396
Joseph Hartley <joseph.hartley@hotmail.com>
2025-09-11 18:30:53 +0800

    get data not working but work done

--- api/api.go
+++ api/api.go
@@ -4,4 +4,16 @@ var Base_url string = "https://data.api.abs.gov.au/"
 
 var DataflowURL string = "rest/dataflow/"
 
-var DataflowHeader string = "Accept: application/vnd.sdmx.structure+json"
+var DataURL string = "rest/data/ABS"
+
+var DataflowHeader string = "application/vnd.sdmx.structure+json"
+
+var XmlHeader string = "application/xml"
+
+var XmlStructureHeader string = "application/vnd.sdmx.structurespecificdata+xml"
+
+var JSONHeader string = "application/vnd.sdmx.data+json"
+
+var CSVHeader string = "application/vnd.sdmx.data+csv"
+
+var CSVLabelHeader string = "application/vnd.sdmx.data+csv;labels=both"
